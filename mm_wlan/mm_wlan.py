bceb9e59ae17010bd6ffbbeb61e6950d6cb7f65a
javacasm <javacasm@gmail.com>
2024-05-09 13:10:45 +0200

    Fix error when using ESP32

--- mm_wlan/mm_wlan.py
+++ mm_wlan/mm_wlan.py
@@ -7,13 +7,14 @@ wireless network. https://github.com/monkmakes/mm_wlan
 """
 
 
-import network, time
+import network, time, sys
 
 wlan = network.WLAN(network.STA_IF)
 
 def connect_to_network(ssid, password, retries=10, verbose=True):
     wlan.active(True)
-    wlan.config(pm = 0xa11140)  # Disable power-save mode
+    if sys.platform != 'esp32':
+        wlan.config(pm = 0xa11140)  # Disable power-save mode
     wlan.connect(ssid, password)
     if verbose: print('Connecting to ' + ssid, end=' ')
         
